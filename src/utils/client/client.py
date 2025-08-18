79baebd81b73fe5a573992bbcdb1a4fdecf66955
Vlad-Yarko <vladyarik88888@gmail.com>
2025-08-19 01:55:59 +0300

    fix(utils): client_session bug

--- src/utils/client/client.py
+++ src/utils/client/client.py
@@ -21,18 +21,12 @@ class Client(ABC):
 def client_session(func):
     @wraps(func)
     async def wrapper(self, *args, **kwargs):
-        if self.client.client_type == "http":
-            session_was_already_open = self.client.session is not None and not self.client.session.closed
-            if not session_was_already_open:
-                await self.client.open_session()
-            try:
-                return await func(self, *args, **kwargs)
-            finally:
-                if not session_was_already_open:
-                    await self.client.close_session()
-        else:
-            session_was_already_open = self.client.device is not None
-            if not session_was_already_open:
-                await self.client.open_session()
+        session_was_already_open = self.client.session is not None and not self.client.session.closed
+        if not session_was_already_open:
+            await self.client.open_session()
+        try:
             return await func(self, *args, **kwargs)
+        finally:
+            if not session_was_already_open:
+                await self.client.close_session()
     return wrapper
