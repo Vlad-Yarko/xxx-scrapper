9c922fabbca3d6b336db7aacaee85322328119c0
Vlad-Yarko <vladyarik88888@gmail.com>
2025-08-19 02:42:12 +0300

    feat: add test to main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from xxx-scrapper!")
+import asyncio
+
+from src.utils.logger import logger
+from src.services import OLXService
+from src.clients.api import OLXClient
+
+
+async def main() -> None:
+    service = OLXService(
+        olx_client=OLXClient()
+    )
+    data = await service.get_products()
+    logger.info(f"PRODUCTS: {data}")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
