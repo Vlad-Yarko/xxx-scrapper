ff628ca28b3b9d75f15f17aac565130e38b43120
Vlad-Yarko <vladyarik88888@gmail.com>
2025-08-22 01:32:27 +0300

    test(shafa): state filter

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import json
 from src.utils.logger import logger
 from src.services import OLXService, ShafaService
 from src.clients.api import OLXClient, ShafaClient
+from src.enums.olx import StateEnum as OLXStateEnum
+from src.enums.shafa import StateEnum as ShafaStateEnum
 
 
 async def main() -> None:
@@ -13,11 +15,11 @@ async def main() -> None:
     shafa_service = ShafaService(
         shafa_client=ShafaClient()
     )
-    olx_data = await olx_service.get_products(query="кросівки", filter_float_price_from=10000, filter_state_enum="used", filter_float_size_from="35")
+    olx_data = await olx_service.get_products(query="кросівки", filter_float_price_from=10000, filter_state_enum=OLXStateEnum.new.value, filter_float_size_from="35")
     with open("olx_data.json", "w", encoding="utf-8") as file:
         json.dump(olx_data, file, ensure_ascii=False, indent=4)
-    shafa_data_all = await shafa_service.get_products_all(search_text="кросівки", price_from=50, price_to=100)
-    shafa_data_top = await shafa_service.get_products_top(search_text="кросівки", price_from=50, price_to=100)
+    shafa_data_all = await shafa_service.get_products_all(search_text="кросівки", price_from=50, price_to=100, state=ShafaStateEnum.used.value)
+    shafa_data_top = await shafa_service.get_products_top(search_text="кросівки", price_from=50, price_to=100, state=ShafaStateEnum.used.value)
     with open("shafa_all.json", "w", encoding="utf-8") as file:
         json.dump(shafa_data_all, file, ensure_ascii=False, indent=4)
     with open("shafa_top.json", "w", encoding="utf-8") as file:
