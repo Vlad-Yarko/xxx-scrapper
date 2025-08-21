e7b1c5aaa7317594af4b0742b4eaba3be39e1d12
Vlad-Yarko <vladyarik88888@gmail.com>
2025-08-21 15:08:04 +0300

    feat(clients): olx api client add size filter

--- src/clients/api/olx.py
+++ src/clients/api/olx.py
@@ -16,9 +16,11 @@ class OLXClient(ProductClient, JSONClient):
         query: str,
         offset: int = 0,
         limit: int = 40,
-        filter_float_price_from: Optional[str] = None,
-        filter_float_price_to: Optional[str] = None,
-        filter_state_enum: Optional[StateEnum] = None
+        filter_float_price_from: Optional[int] = None,
+        filter_float_price_to: Optional[int] = None,
+        filter_state_enum: Optional[StateEnum] = None,
+        filter_float_size_from: Optional[str] = None,
+        filter_float_size_to: Optional[str] = None
     ) -> Union[None, dict, list]:
         self.endpoint = URLEnum.PRODUCTS_ENDPOINT_URL.value
         self.params = {
@@ -29,7 +31,9 @@ class OLXClient(ProductClient, JSONClient):
         optional_params = {
             "filter_float_price:from": filter_float_price_from,
             "filter_float_price:to": filter_float_price_to,
-            "filter_enum_state[0]": filter_state_enum
+            "filter_enum_state[0]": filter_state_enum,
+            "filter_float_size:from": filter_float_size_from,
+            "filter_float_size:to": filter_float_size_to
         }
         self.params.update({k: v for k, v in optional_params.items() if v is not None})
         data = await self.get()
