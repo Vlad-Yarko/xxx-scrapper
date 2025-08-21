8e698f0204af6ed9097e2b06b4a8d6d46db01d8c
Vlad-Yarko <vladyarik88888@gmail.com>
2025-08-21 15:32:35 +0300

    feat(clients): shafa api client add price filters

--- src/clients/api/shafa.py
+++ src/clients/api/shafa.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Optional
 
 from src.utils.client import JSONClient
 from src.clients.product import ProductClient
@@ -18,7 +18,9 @@ class ShafaClient(ProductClient, JSONClient):
         self,
         search_text: str,
         page_enum: int = 1,
-        first: int = 44
+        first: int = 44,
+        price_from: Optional[int] = None,
+        price_to: Optional[int] = None
     ) -> Union[None, dict, list]:
         self.endpoint = URLEnum.PRODUCTS_ENDPOINT_URL.value
         self.payload = {
@@ -39,6 +41,11 @@ class ShafaClient(ProductClient, JSONClient):
             },
             "query": "query WEB_CatalogProducts($first: Int!, $pageNum: Int!, $catalogSlug: String, $brands: [Int], $orderBy: String, $sizes: [Int], $conditions: [Int], $colors: [Int], $priceTo: Int, $priceFrom: Int, $priceId: [Int], $ukrainian: Boolean, $searchText: String, $freeShipping: Boolean, $isOnSale: Boolean, $characteristics: [Int!], $cities: [Int!], $lastViewedProductId: Int, $indexedFilter: IndexedFilterInput) {\n  products(\n    first: $first\n    pageNum: $pageNum\n    orderBy: $orderBy\n    sizes: $sizes\n    condition: $conditions\n    colors: $colors\n    priceTo: $priceTo\n    priceFrom: $priceFrom\n    catalogSlug: $catalogSlug\n    brands: $brands\n    priceId: $priceId\n    ukrainian: $ukrainian\n    searchText: $searchText\n    freeShipping: $freeShipping\n    isOnSale: $isOnSale\n    characteristics: $characteristics\n    cities: $cities\n    lastViewedProductId: $lastViewedProductId\n    indexedFilter: $indexedFilter\n  ) {\n    mostFrequentCatalogSlug\n    topLevelCategories {\n      id\n      name\n      slug\n      countOfProducts\n      __typename\n    }\n    edges {\n      node {\n        id\n        ...productCardFeedData\n        __typename\n      }\n      __typename\n    }\n    pageInfo {\n      endCursor\n      hasNextPage\n      total\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment productCardFeedData on Product {\n  id\n  url\n  thumbnail\n  name\n  price\n  oldPrice\n  statusTitle\n  discountPercent\n  ...productLikes\n  brand {\n    id\n    name\n    __typename\n  }\n  catalogSlug\n  isNew\n  sizes {\n    id\n    name\n    __typename\n  }\n  saleLabel {\n    status\n    date\n    price\n    __typename\n  }\n  freeDeliveryServices\n  isUkrainian\n  ownerHasRecentActivity\n  tags\n  rating\n  ratingAmount\n  seller {\n    id\n    isTopSeller\n    __typename\n  }\n  isViewed\n  createdAt\n  sellingCondition\n  __typename\n}\n\nfragment productLikes on Product {\n  likes\n  isLiked\n  __typename\n}"
         }
+        optional_params = {
+            "priceFrom": price_from,
+            "priceTo": price_to
+        }
+        self.payload["variables"].update({k: v for k, v in optional_params.items() if v is not None})
         data = await self.post()
         return data
 
