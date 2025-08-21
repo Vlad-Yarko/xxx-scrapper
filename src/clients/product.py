85e1d87787460a554b5222f9e7e57b8ba87d7282
Vlad-Yarko <vladyarik88888@gmail.com>
2025-08-21 15:48:53 +0300

    fix(clients): shafa api client fetch methods

--- src/clients/product.py
+++ src/clients/product.py
@@ -5,3 +5,9 @@ class ProductClient:
     @abstractmethod
     async def get_products(*args, **kwargs) -> None:
         raise NotImplementedError()
+
+    async def get_products_all(*args, **kwargs) -> None:
+        raise NotImplementedError()
+
+    async def get_products_top(*args, **kwargs) -> None:
+        raise NotImplementedError()
