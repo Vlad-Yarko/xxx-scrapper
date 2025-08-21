1bafc84eec78c2835b5980b0400a310589351daf
Vlad-Yarko <vladyarik88888@gmail.com>
2025-08-21 14:59:44 +0300

    feat(clients): olx api client add state filter

--- src/clients/api/olx.py
+++ src/clients/api/olx.py
@@ -2,7 +2,7 @@ from typing import Union, Optional
 
 from src.utils.client import JSONClient
 from src.clients.product import ProductClient
-from src.enums.olx import URLEnum
+from src.enums.olx import URLEnum, StateEnum
 
 
 class OLXClient(ProductClient, JSONClient):
@@ -17,7 +17,8 @@ class OLXClient(ProductClient, JSONClient):
         offset: int = 0,
         limit: int = 40,
         filter_float_price_from: Optional[str] = None,
-        filter_float_price_to: Optional[str] = None
+        filter_float_price_to: Optional[str] = None,
+        filter_state_enum: Optional[StateEnum] = None
     ) -> Union[None, dict, list]:
         self.endpoint = URLEnum.PRODUCTS_ENDPOINT_URL.value
         self.params = {
@@ -28,6 +29,7 @@ class OLXClient(ProductClient, JSONClient):
         optional_params = {
             "filter_float_price:from": filter_float_price_from,
             "filter_float_price:to": filter_float_price_to,
+            "filter_enum_state[0]": filter_state_enum
         }
         self.params.update({k: v for k, v in optional_params.items() if v is not None})
         data = await self.get()
