5395d441b7f17777081e5f9892b66ab7a30b393b
Vlad-Yarko <vladyarik88888@gmail.com>
2025-08-22 01:15:23 +0300

    feat(enums): add shafa state enum

--- src/enums/shafa.py
+++ src/enums/shafa.py
@@ -4,3 +4,8 @@ import enum
 class URLEnum(str, enum.Enum):
     BASE_URL = "https://shafa.ua/api/v3/"
     PRODUCTS_ENDPOINT_URL = "graphiql"
+
+
+class StateEnum(enum.Enum):
+    new = [1]
+    used = [2, 3, 4, 5]
