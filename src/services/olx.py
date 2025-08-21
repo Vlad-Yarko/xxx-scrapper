ed92b7690f99d9bc4012fcc9025269651a279a69
Vlad-Yarko <vladyarik88888@gmail.com>
2025-08-21 15:46:07 +0300

    feat(services): update olx service fetch method

--- src/services/olx.py
+++ src/services/olx.py
@@ -1,5 +1,8 @@
+from typing import Optional
+
 from src.services.product import ProductService
 from src.clients import ProductClient
+from src.enums.olx import StateEnum
 
 
 class OLXService(ProductService):
@@ -10,3 +13,26 @@ class OLXService(ProductService):
         super().__init__(
             client=olx_client
         )
+
+    async def get_products(
+            self,
+            query: str,
+            offset: int = 0,
+            limit: int = 40,
+            filter_float_price_from: Optional[int] = None,
+            filter_float_price_to: Optional[int] = None,
+            filter_state_enum: Optional[StateEnum] = None,
+            filter_float_size_from: Optional[str] = None,
+            filter_float_size_to: Optional[str] = None
+        ) -> dict | None:
+        data =  await super().get_products(
+            query=query,
+            offset=offset,
+            limit=limit,
+            filter_float_price_from=filter_float_price_from,
+            filter_float_price_to=filter_float_price_to,
+            filter_state_enum=filter_state_enum,
+            filter_float_size_from=filter_float_size_from,
+            filter_float_size_to=filter_float_size_to
+        )
+        return data
