63521a9db54e1ca391e59648403c360f7e8baccf
Vlad-Yarko <vladyarik88888@gmail.com>
2025-08-21 14:58:13 +0300

    feat(enums): add olx state enum

--- src/enums/olx.py
+++ src/enums/olx.py
@@ -4,3 +4,8 @@ import enum
 class URLEnum(str, enum.Enum):
     BASE_URL = "https://www.olx.ua/api/v1/"
     PRODUCTS_ENDPOINT_URL = "offers/"
+
+
+class StateEnum(str, enum.Enum):
+    used = "used"
+    new = "new"
